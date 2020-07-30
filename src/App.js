35a153d6fe3af106cf6b1429aee3a8daf0db8278
juamecos <juamecos@gmail.com>
2020-07-30 05:47:43 +0200

    react router / custom layouts / footer component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,35 @@
 import React from "react"
 import Header from "./components/Header"
 import Homepage from "./pages/Hompepage"
+import Registration from "./pages/Registration"
 
 import "./default.scss"
+import { Route, Switch } from "react-router"
+import MainLayout from "./layouts/MainLayout"
+import HomepageLayout from "./layouts/HomepageLayout"
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <div className="main">
-        <Homepage />
-      </div>
+      <Switch>
+        <Route
+          exact
+          path="/"
+          render={() => (
+            <HomepageLayout>
+              <Homepage />
+            </HomepageLayout>
+          )}
+        />
+        <Route
+          path="/registration"
+          render={() => (
+            <MainLayout>
+              <Registration />
+            </MainLayout>
+          )}
+        />
+      </Switch>
     </div>
   )
 }
---
+++ src/components/Footer/index.js
@@ -0,0 +1,12 @@
+import React from "react"
+import "./styles.scss"
+
+const Footer = props => {
+  return (
+    <footer className="footer">
+      <div className="wrap">&copy; Juan José Mena Costa</div>
+    </footer>
+  )
+}
+
+export default Footer
